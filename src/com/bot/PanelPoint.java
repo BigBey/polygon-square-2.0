cb4ab4a586433f783e3653a71ed6c9a00625525f
BigBey <bey.sviatoslav@yandex.ru>
2017-04-19 00:35:22 +0400

    dgd
    dfgd

--- src/com/bot/PanelPoint.java
+++ src/com/bot/PanelPoint.java
@@ -6,15 +6,16 @@ import javax.swing.*;
  * Created by Слава on 16.04.2017.
  */
 public class PanelPoint extends JPanel{
+    int n;
+    JLabel X = new JLabel("X"+n+":");
+    JLabel Y =new JLabel("Y"+n+":");
+    JTextField x = new JTextField();
+    JTextField y = new JTextField();
     PanelPoint(int n){
-        final JLabel X = new JLabel("X"+n+":");
-        final JLabel Y =new JLabel("Y"+n+":");
         X.setBounds(0,0, 20,30);
         Y.setBounds(60,0,20,30);
         add(X);
         add(Y);
-        final JTextField x = new JTextField();
-        final JTextField y = new JTextField();
         x.setBounds(20,0,20,30);
         y.setBounds(80,0,20,30);
         setLayout(null);
@@ -23,5 +24,14 @@ public class PanelPoint extends JPanel{
         System.out.println("called "+n);
         repaint();
         revalidate();
+    }
+
+    public int getX(JTextField x){
+        int x1=(!x.getText().equals("")?Integer.parseInt(x.getText()):0);
+        return x1;
+    }
+    public int getY(JTextField x){
+        int y1=(!y.getText().equals("")?Integer.parseInt(y.getText()):0);
+        return y1;
     }
 }
