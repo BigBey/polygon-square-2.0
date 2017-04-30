c5cc1a412c49e9b7b9d3864c8a794a5a63152b33
BigBey <bey.sviatoslav@yandex.ru>
2017-04-30 21:35:40 +0400

    fhf
    fhfgh

--- src/com/bot/Main.java
+++ src/com/bot/Main.java
@@ -24,13 +24,70 @@ public class Main {
     Scanner fin = null;
     PrintStream fout = null;
 
+
+    public static void addPanelPoints(int k, JPanel p){
+        if (k==0){
+            p.removeAll();
+            p.repaint();
+            p.revalidate();
+            return;
+        }
+        for (int i = 0;i<k;i++) {
+            PanelPoint pp = new PanelPoint(i+1);
+            pp.setBounds(0,i*50,300,50);
+            panelPoints.add(pp);
+            p.add(pp);
+            p.repaint();
+        }
+    }
+    public static void addPanelPoints(int k, JPanel p, Scanner fin){
+        if (k==0){
+            p.removeAll();
+            p.repaint();
+            p.revalidate();
+            return;
+        }
+        for (int i = 0;i<k;i++) {
+            PanelPoint pp = new PanelPoint(i+1, fin.nextInt(), fin.nextInt());
+            pp.setBounds(0,i*50,300,50);
+            panelPoints.add(pp);
+            p.add(pp);
+            p.repaint();
+        }
+    }
+    public static void addPanelPoints(int k, JPanel p, int x0, int y0){
+        double cor = 0.0;
+        double dcor = 360/k;
+        if (k==0){
+            p.removeAll();
+            p.repaint();
+            p.revalidate();
+            return;
+        }
+        for(int i =0;i<k;i++) {
+            System.out.println(cor);
+            int r = random.nextInt(100);
+            int x = x0 + (int)(r * Math.cos(cor/180*Math.PI));
+            int y =y0+(int)(r*Math.sin(cor/180*Math.PI));
+            PanelPoint pp = new PanelPoint(i+1,x,y);
+            pp.setBounds(0,i*50,300,50);
+            panelPoints.add(pp);
+            p.add(pp);
+            p.repaint();
+            cor+=dcor;
+        }
+    }
+
     public static void createGUI() {
         final JFrame frame = new JFrame("Testframe");
 	    frame.setPreferredSize(new Dimension(700,700));
+
 	    JPanel panel = new JPanel(new BorderLayout());
+
         final Panel butPanel = new Panel();
         butPanel.setLayout(null);
         butPanel.setPreferredSize(new Dimension(300,700));
+
         final Panel panelpointpane = new Panel();
         panelpointpane.setLayout(null);
         panelpointpane.setPreferredSize(new Dimension(350,700));
@@ -38,15 +95,19 @@ public class Main {
         final JLabel N = new JLabel("N:");
         N.setBounds(2,90,15,25);
         butPanel.add(N);
+
         final JTextField n = new JTextField();
         n.setBounds(20,90, 25,25);
         butPanel.add(n);
-        final JLabel A = new JLabel("Ответ(площадь многоугольника):");
-        A.setBounds(2, 320, 50, 25);
+
+        final JLabel A = new JLabel("Ответ(площадь мн-ка):");
+        A.setBounds(2, 320, 150, 25);
         butPanel.add(A);
+
         final JTextField a = new JTextField("");
-        a.setBounds(40,320, 100,30);
+        a.setBounds(130,320, 100,30);
         butPanel.add(a);
+
         final JPanel p = new JPanel();
         p.setBounds(0,400,300,400);
         butPanel.add(p);
@@ -54,10 +115,6 @@ public class Main {
         JButton button2 = new JButton("очистить");
         button2.setBounds(2,360,160,30);
         butPanel.add(button2);
-
-        final JButton button3 = new JButton( "Добавить колличество вершин");
-        butPanel.add(button3);
-        button3.setBounds(2,2,250,30);
 
 
         button2.addActionListener(new ActionListener() {
@@ -86,30 +143,22 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        final JButton button3 = new JButton( "Добавить колличество вершин");
+        butPanel.add(button3);
+        button3.setBounds(2,2,250,30);
 
         button3.addActionListener(new ActionListener() {
             @Override
             public  void actionPerformed(ActionEvent e) {
                 int K = (!n.getText().equals("")?Integer.parseInt(n.getText()):0);
-                if (K==0){
-                    p.removeAll();
-                    p.repaint();
-                    p.revalidate();
-                    return;
-                }
-                for (int i = 0;i<K;i++) {
-                    PanelPoint pp = new PanelPoint(i+1);
-                    pp.setBounds(0,i*50,300,50);
-                    panelPoints.add(pp);
-                    p.add(pp);
-                    p.repaint();
-                }
+                addPanelPoints(K, p);
             }
         });
 
         JButton button4 = new JButton("нарисовать многоугольник");
         button4.setBounds(2,220,250,30);
         butPanel.add(button4);
+
         button4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -121,9 +170,11 @@ public class Main {
 
             }
         });
+
         JButton button5 = new JButton("посчитать площадь");
         button5.setBounds(2,270,250,30);
         butPanel.add(button5);
+
         button5.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -133,34 +184,25 @@ public class Main {
                 a.setText(""+s);
             }
         });
+
         JButton button6 = new JButton("задать случайные вершины");
         button6.setBounds(2,50,250,30);
         butPanel.add(button6);
+
         button6.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int k = (!n.getText().equals("")?Integer.parseInt(n.getText()):0);
                 int x0 = random.nextInt(300);
                 int y0 = random.nextInt(300);
-                double cor = 0.0;
-                double dcor = 360/k;
-                for(int i =0;i<k;i++) {
-                    System.out.println(cor);
-                    int r = random.nextInt(100);
-                    int x = x0 + (int)(r * Math.cos(cor/180*Math.PI));
-                    int y =y0+(int)(r*Math.sin(cor/180*Math.PI));
-                    PanelPoint pp = new PanelPoint(i+1,x,y);
-                    pp.setBounds(0,i*50,300,50);
-                    panelPoints.add(pp);
-                    p.add(pp);
-                    p.repaint();
-                    cor+=dcor;
-                }
+                addPanelPoints(k, p ,x0,y0);
             }
         });
+
         JButton button7 = new JButton("чтение из файла");
         button7.setBounds(2,130,160,30);
         butPanel.add(button7);
+
         button7.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -172,13 +214,7 @@ public class Main {
                     int k = fin.nextInt();
                     n.setText(""+k);
                     panelPoints.clear();
-                    for(int i = 0;i<k;i++){
-                        PanelPoint pp = new PanelPoint(i+1, fin.nextInt(), fin.nextInt());
-                        pp.setBounds(0,i*50,300,50);
-                        panelPoints.add(pp);
-                        p.add(pp);
-                        p.repaint();
-                    }
+                    addPanelPoints(k, p, fin);
                 }catch(FileNotFoundException e1){
                     System.out.println("не найден файл " + e);
                 }finally {
@@ -191,9 +227,11 @@ public class Main {
                 }
             }
         });
+
         JButton button8 = new JButton("вывод в файл");
         button8.setBounds(2,180,160,30);
         butPanel.add(button8);
+
         button8.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
